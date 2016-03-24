27bb770b22f70a07edb2c699e90d0c2ddc9d8ef8
tbehm96 <trevor.behm@cuw.edu>
2016-03-24 09:49:36 -0500

    hmwk 1 bts

--- app/src/main/java/com/example/awesomefat/bst_csc300_spring2016/BST.java
+++ app/src/main/java/com/example/awesomefat/bst_csc300_spring2016/BST.java
@@ -42,33 +42,48 @@ public class BST
         {
             childStack.push(this.root);
             while(!childStack.isEmpty() || !tempStack.isEmpty())
-            {
+            { //System.out.println("A");
                 //clear child stack by adding its children to tempStack and then popping
+                if (childStack.peek().getLeftTree() != null)
+                    tempStack.push(childStack.peek().getLeftTree());
+                if (childStack.peek().getRightTree() != null)
+                    tempStack.push(childStack.peek().getRightTree());
+                childStack.pop();
                 while(!childStack.isEmpty())
-                {
+                { //System.out.println("B");
                     //push the left and right children of peek childStack onto tempStack
                     //pop from childStack and push onto contentStack
+                    if (childStack.peek().getLeftTree() != null)
+                        tempStack.push(childStack.peek().getLeftTree());
+                    if (childStack.peek().getRightTree() != null)
+                        tempStack.push(childStack.peek().getRightTree());
+                    contentStack.push(childStack.pop());
                 }
 
                 //move contents of tempStack onto childStack in REVERSE
                 while(!tempStack.isEmpty())
-                {
+                { //System.out.println("C");
                     //pop -> push onto childStack
+                    childStack.push(tempStack.pop());
                 }
             }
 
             //we now know that child and temp stacks are empty and all of our
             //nodes are in contentStack in REVERSE order, so we need to reverse them again
             while(!contentStack.isEmpty())
-            {
+            { //System.out.println("D");
                 //pop -> push onto answerStack
+                answerStack.push(contentStack.pop());
             }
 
             //our final answer is answerStack, we can visit the nodes in pop order
+            System.out.println("**** Level Order Display ****");
             while(!answerStack.isEmpty())
             {
                 //pop and display value
+                System.out.println(answerStack.pop().getPayload());
             }
+            System.out.println("**** Level Order Display End ****");
         }
     }
 
